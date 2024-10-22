d00e3c556fd861c0421a4be1f3df464ec2f8ba6d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-10-22 14:52:09 -0400

    feat: add boilerplate for the functions
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

---
+++ functions/queue_actions/app.py
@@ -0,0 +1,12 @@
+import boto3
+import os
+
+def lambda_handler(event, context):
+    sqs = boto3.resource('sqs')
+    queue_name = os.environ['QUEUE_NAME']
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+    response = queue.send_message(
+        MessageBody='Hello from Lambda!'
+    )
+    return response
+
